154d72724a5cd862038ffb1434b92844bab60c22
Misaka-L <lipww1234@foxmail.com>
2024-01-21 12:23:27 +0800

    feature: add cloudflare web analytics

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <NuxtPwaManifest />
-  <Title>VRCD</Title>
   <ClientOnly>
     <PromptForUpdate />
   </ClientOnly>
@@ -16,3 +15,12 @@
     </footer>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'VRCD',
+  script: [
+    { defer: true, src: 'https://static.cloudflareinsights.com/beacon.min.js', 'data-cf-beacon': '{"token": "0f994579ee6148399fc27afea23abec3"}' }
+  ]
+})
+</script>
