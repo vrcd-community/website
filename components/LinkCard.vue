2aee30df96269e4d4eb887b9f04e44ba3ad5ace3
TheresaQWQ <yxiao2044@gmail.com>
2024-02-18 12:28:35 +0800

    feat: 优化一下首页链接按钮的动画

--- components/LinkCard.vue
+++ components/LinkCard.vue
@@ -1,8 +1,8 @@
 <!--链接卡片-->
 <template>
   <NuxtLink :to="to" :href="href" :target="target"
-    class="rounded-lg flex justify-start items-center transition hover:drop-shadow-[0px_0px_8px_#00000020] dark:hover:drop-shadow-[0px_0px_8px_#ffffff20]">
-    <div class="py-4 min-w-64 px-4 rounded-md flex-1 justify-center bg-zinc-100/90 dark:bg-zinc-900/90">
+    class="rounded-lg flex justify-start items-center transition vrcd-link-button">
+    <div class="py-4 min-w-64 px-4 rounded-md flex-1 justify-center bg-zinc-100/90 dark:bg-zinc-900/90 vrcd-link-button-content">
       <h2 class="text-2xl font-semibold">
         {{ title }}
       </h2>
@@ -13,7 +13,7 @@
     <span class="ml-3 sm:hidden flex text-base font-semibold text-zinc-100/90 dark:text-zinc-900/90">
       了解更多
     </span>
-    <Icon class="text-xl mx-1 text-white dark:text-black" name="material-symbols:arrow-forward-ios-rounded" />
+    <Icon class="text-xl mx-1 text-white dark:text-black vrcd-link-button-icon" name="material-symbols:arrow-forward-ios-rounded" />
   </NuxtLink>
 </template>
 
@@ -38,3 +38,22 @@ defineProps<{
   target?: string
 }>()
 </script>
+
+<style scoped>
+.vrcd-link-button {
+  box-shadow: 1px 1px 4px 1px #00000039;
+}
+
+.vrcd-link-button .vrcd-link-button-icon {
+  transition: all .2s ease-in-out;
+  width: 20px;
+}
+
+.vrcd-link-button .vrcd-link-button-content {
+  box-shadow: 0px 2px 4px 1px #00000039;
+}
+
+.vrcd-link-button:hover .vrcd-link-button-icon {
+  width: 30px;
+}
+</style>
