f85f94e16142b22c77d4e41750a051f917e52cae
Misaka-L <lipww1234@foxmail.com>
2024-04-05 15:48:31 +0800

    feature: search api adjust

--- components/SearchResultItem.vue
+++ components/SearchResultItem.vue
@@ -1,36 +1,11 @@
 <script setup lang="ts">
-const props = defineProps<{
+defineProps<{
   result: SearchHit
 }>()
-
-// https://github.com/mdit-vue/mdit-vue/blob/d493d4ca4cf5af4c54cca56a04b4bae5b8700f0e/packages/shared/src/slugify.ts
-const rControl = /[\u0000-\u001f]/g;
-const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'“”‘’<>,.?/]+/g;
-const rCombining = /[\u0300-\u036F]/g;
-
-function slugify(str: string): string {
-  return str
-    .normalize('NFKD')
-    // Remove accents
-    .replace(rCombining, '')
-    // Remove control characters
-    .replace(rControl, '')
-    // Replace special characters
-    .replace(rSpecial, '-')
-    // Remove continuos separators
-    .replace(/-{2,}/g, '-')
-    // Remove prefixing and trailing separators
-    .replace(/^-+|-+$/g, '')
-    // ensure it doesn't start with a number (#121)
-    .replace(/^(\d)/, '_$1')
-    // lowercase
-    .toLowerCase();
-}
 </script>
 
 <template>
-  <NuxtLink target="_blank"
-    :to="'https://docs.vrczh.org/' + result.uri.replace('.md', '') + '#' + slugify(result.chunkTitle)" class="block">
+  <NuxtLink target="_blank" :to="result.uri" class="block">
     <div class="bg-white/50 dark:bg-neutral-900/40 shadow-sm rounded-md p-4 transition">
       <h2 class="text-2xl font-semibold">{{ result.title }} > {{ result.chunkTitle }}</h2>
       <MDC
--- server/api/search.get.ts
+++ server/api/search.get.ts
@@ -19,8 +19,38 @@ export default defineEventHandler(async (event) => {
       chunkContent: hit._source.chunk.content,
       chunkTitle: hit._source.chunk.title,
       chunkLevel: hit._source.chunk.level,
-      uri: hit._source.extra.path,
+      uri:
+        "https://docs.vrczh.org/" +
+        hit._source.extra.path.replace(".md", "") +
+        "#" +
+        slugify(hit._source.chunk.title),
       source: hit._source.extra.source,
     })),
   } as SearchResult
 })
+
+// https://github.com/mdit-vue/mdit-vue/blob/d493d4ca4cf5af4c54cca56a04b4bae5b8700f0e/packages/shared/src/slugify.ts
+const rControl = /[\u0000-\u001f]/g
+const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'“”‘’<>,.?/]+/g
+const rCombining = /[\u0300-\u036F]/g
+
+function slugify(str: string): string {
+  return (
+    str
+      .normalize("NFKD")
+      // Remove accents
+      .replace(rCombining, "")
+      // Remove control characters
+      .replace(rControl, "")
+      // Replace special characters
+      .replace(rSpecial, "-")
+      // Remove continuos separators
+      .replace(/-{2,}/g, "-")
+      // Remove prefixing and trailing separators
+      .replace(/^-+|-+$/g, "")
+      // ensure it doesn't start with a number (#121)
+      .replace(/^(\d)/, "_$1")
+      // lowercase
+      .toLowerCase()
+  )
+}
