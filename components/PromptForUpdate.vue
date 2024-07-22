069897d6cd9457ceec0e7c522d366e1fe366a343
Misaka-L <lipww1234@foxmail.com>
2024-07-22 22:40:56 +0800

    fix: prompt for update cover by link button

--- components/PromptForUpdate.vue
+++ components/PromptForUpdate.vue
@@ -9,13 +9,13 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="fixed bottom-0 right-0 bg-zinc-100 dark:bg-zinc-900 m-4 shadow-2xl p-4 rounded-md space-y-2"
+  <div class="fixed bottom-0 right-0 bg-zinc-100 dark:bg-zinc-900 m-4 shadow-2xl p-4 rounded-md space-y-2 z-50"
     v-show="$pwa.needRefresh">
     <p class="text-base">
       有新的内容可用，点击 "刷新" 按钮更新。
     </p>
 
-    <button class="link-button text-sm p-2 dark:bg-gray-700 dark:border-gray-700 bg-gray-300 border-gray-300"
+    <button class="link-button text-sm p-1 dark:bg-gray-700 dark:border-gray-700 bg-gray-300 border-gray-300"
       @click="$pwa.updateServiceWorker()">
       刷新
     </button>
